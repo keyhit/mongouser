b80b5ceb81835c102bc76b4192d4e218ddd5ef65
keyhit <viacheslavkarpenkoju@gmail.com>
2019-06-03 13:03:58 +0300

    Changed seeds adn search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
-  # include Mongoid::MultiParameterAttributes
   field :first_name, type: String
   field :last_name, type: String
   field :birthday, type: Date
@@ -19,9 +18,10 @@ class User
       any_of(
         { first_name: /#{search_value.downcase}/i },
         { last_name: /#{search_value.downcase}/i },
+        { birthday: /Date.parse("#{search_value}"")/i },
         { address: /#{search_value.downcase}/i }
        )
     end
   end
 end
-#{ birthday: /#{search_value.downcase}/i}
+5457427
